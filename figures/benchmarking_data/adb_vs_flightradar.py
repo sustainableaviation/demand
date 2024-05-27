8b8ab79f2c620315697ad0d83779fad23cd6101c
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2024-05-26 21:27:38 -0400

    updated figures

---
+++ figures/benchmarking_data/adb_vs_flightradar.py
@@ -0,0 +1,139 @@
+#%%
+# runs code as interactive cell 
+# https://code.visualstudio.com/docs/python/jupyter-support-py
+
+# IMPORTS #######################################
+
+# plotting
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+# time manipulation
+from datetime import datetime
+# data science
+import numpy as np
+import pandas as pd
+# i/o
+from pathlib import Path, PosixPath, PurePath
+
+# SETUP #########################################
+
+# unit conversion
+cm = 1/2.54 # for inches-cm conversion
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Arial",
+    "font.sans-serif": "Computer Modern",
+    'font.size': 16
+})
+
+# DATA IMPORT ###################################
+
+df_adb = pd.read_excel(
+    io = 'data/data.xlsx',
+    sheet_name = 'ADB',
+    parse_dates=['DateTime'],
+    usecols = lambda column: column in [
+        'DateTime',
+        'Flights'
+    ],
+    header = 0,
+    engine = 'openpyxl'
+)
+
+df_fr = pd.read_excel(
+    io = 'data/data.xlsx',
+    sheet_name = 'Flightradar',
+    parse_dates=['DateTime'],
+    usecols = lambda column: column in [
+        'DateTime',
+        'Flights'
+    ],
+    header = 0,
+    engine = 'openpyxl'
+)
+
+# DATA MANIPULATION #############################
+
+# FIGURE ########################################
+
+# SETUP ######################
+
+fig, ax = plt.subplots(
+    num = 'main',
+    nrows = 1,
+    ncols = 1,
+    dpi = 300,
+    figsize=(30*cm, 10*cm), # A4=(210x297)mm,
+)
+
+# AXIS SCALING ###############
+
+# AXIS LIMITS ################
+
+ax.set_xlim(pd.Timestamp('2023-05-01'), pd.Timestamp('2024-04-30'))
+ax.set_ylim(0, 140000)
+
+# TICKS AND LABELS ###########
+
+import matplotlib.ticker as ticker
+def thousand_formatter(value, tick_number):
+    """
+    Formats the tick label with thousand separators: 1000 = 1'000.
+    """
+    return f"{int(value):,}".replace(",", "'")
+
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(thousand_formatter))
+
+ax.tick_params(axis="x",direction="in", pad=-70)
+plt.xticks(rotation=90)
+
+x_ticks = ax.xaxis.get_major_ticks()
+x_ticks[0].label1.set_visible(False)
+
+# GRIDS ######################
+
+ax.grid(which='both', axis='y', linestyle='-', linewidth = 0.5)
+ax.grid(which='both', axis='x', linestyle='-', linewidth = 0.5)
+
+# AXIS LABELS ################
+
+ax.set_ylabel("Global Air Traffic [\# of flights]")
+
+# PLOTTING ###################
+
+
+ax.plot(
+    df_air['DateTime'],
+    df_air['Flights'],
+    color = 'black',
+    linestyle = '-',
+    linewidth = 1,
+    label = 'ADB (our data)'
+)
+ax.plot(
+    df_fr['DateTime'],
+    df_fr['Flights'],
+    color = 'blue',
+    linestyle = '--',
+    linewidth = 1,
+    label = 'FlightRadar24'
+)
+
+# LEGEND ####################
+
+ax.legend(
+    loc = 'center left',
+)
+
+# EXPORT #########################################
+
+path_current_directory: PosixPath = Path(__file__).parent
+path_figure: PosixPath = path_current_directory / (Path(__file__).stem + '.pdf')
+
+plt.savefig(
+    fname = path_figure,
+    format="pdf",
+    bbox_inches='tight',
+    transparent = False
+)
---
+++ figures/maps_case_study_routes/nigeria.py
@@ -0,0 +1,183 @@
+#%%
+# runs code as interactive cell 
+# https://code.visualstudio.com/docs/python/jupyter-support-py
+
+# IMPORTS #######################################
+
+# plotting
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+# geographic plotting
+import geopandas as gpd
+import contextily as cx
+import xyzservices.providers as xyz
+# time manipulation
+from datetime import datetime
+# data science
+import numpy as np
+import pandas as pd
+# i/o
+from pathlib import Path, PosixPath, PurePath
+
+# SETUP #########################################
+
+# unit conversion
+cm = 1/2.54 # for inches-cm conversion
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Arial",
+    "font.sans-serif": "Computer Modern",
+    'font.size': 16
+})
+
+# DATA IMPORT ###################################
+
+countries = gpd.read_file('../_geodata/ne_10m_admin_0_countries')
+water = gpd.read_file('../_geodata/ne_10m_rivers_lake_centerlines')
+urban = gpd.read_file('../_geodata/ne_10m_urban_areas')
+graticules = gpd.read_file('../_geodata/ne_50m_graticules_10')
+admin = gpd.read_file('../_geodata/ne_10m_admin_0_countries')
+oceans = gpd.read_file('../_geodata/ne_10m_ocean')
+
+calculated_route = gpd.read_file('data/Routes_noSlopes/Nigeria.shp')
+
+# GEOGRAPHY SETUP ###############################
+
+# https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoDataFrame.set_crs.html
+target_projection = "ESRI:102022" # https://epsg.io/26392
+
+countries = countries.to_crs(target_projection)
+water = water.to_crs(target_projection)
+urban = urban.to_crs(target_projection)
+graticules = graticules.to_crs(target_projection)
+admin = admin.to_crs(target_projection)
+#oceans = oceans.to_crs(target_projection)
+calculated_route = calculated_route.to_crs(target_projection)
+
+# https://geopandas.org/en/stable/docs/reference/api/geopandas.points_from_xy.html#geopandas-points-from-xy
+# coordinated can best be selected at:
+# https://epsg.io/map
+lower_left = gpd.points_from_xy(
+    x = [2.6], # longitude
+    y = [5.725311], # latitude
+    crs='EPSG:4326' # = WGS 84
+).to_crs(target_projection)
+upper_right = gpd.points_from_xy(
+    x = [7.7], # longitude
+    y = [9.3], # latitude
+    crs='EPSG:4326' # = WGS 84
+).to_crs(target_projection)
+
+# DATA MANIPULATION #############################
+
+# FIGURE ########################################
+
+# SETUP ######################
+
+fig, ax = plt.subplots(
+    num = 'main',
+    nrows = 1,
+    ncols = 1,
+    dpi = 500,
+    figsize=(10*cm, 10*cm), # A4=(210x297)mm,
+)
+
+"""
+# oceans inverted after projection transformation
+# https://stackoverflow.com/questions/78536561
+oceans.plot(
+    ax = ax,
+    color = 'lightblue',
+    alpha = 1,
+)
+"""
+urban.plot(
+    ax = ax,
+    color = 'orange',
+    alpha = 1,
+)
+water.plot(
+    ax = ax,
+    color = 'lightblue',
+    linewidth = 0.75,
+    alpha = 1,
+)
+countries.plot(
+    ax = ax,
+    facecolor = 'none', # must be "none" instead of None, https://stackoverflow.com/a/47847586
+    edgecolor = 'black',
+    alpha = 1,
+    linewidth = 0.5
+)
+
+# DATA #######################
+
+# AXIS LIMITS ################
+
+ax.set_xlim(
+    lower_left.x[0],
+    upper_right.x[0]
+)
+
+ax.set_ylim(
+    lower_left.y[0],
+    upper_right.y[0]
+)
+
+# TICKS AND LABELS ###########
+
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+
+# GRIDS ######################
+
+# AXIS LABELS ################
+
+# PLOTTING ###################
+
+calculated_route.plot(
+    ax = ax,
+    color = 'red',
+    linewidth = 1,
+    alpha = 1,
+    label = 'Calculated Route'
+)
+
+# MAP BACKGROUND #############
+
+# https://contextily.readthedocs.io/en/latest/working_with_local_files.html#Reading-with-add_basemap
+cx.add_basemap(
+    ax,
+    source='../_geodata/GRAY_HR_SR_W/GRAY_HR_SR_W.tif',
+    crs=target_projection
+)
+# alternatively, tiles could be downloaded from different providers
+# using the xyzservices library
+# https://geopandas.org/en/stable/gallery/plotting_basemap_background.html#Adding-a-background-map-to-plots
+
+# LEGEND #####################
+
+# https://github.com/ppinard/matplotlib-scalebar?tab=readme-ov-file#scalebar-arguments
+from matplotlib_scalebar.scalebar import ScaleBar
+ax.add_artist(
+    ScaleBar(
+        dx=1,
+        location='lower right',
+        box_alpha=0.5,
+    )
+)
+
+# EXPORT #########################################
+
+path_current_directory: PosixPath = Path(__file__).parent
+path_figure: PosixPath = path_current_directory / (Path(__file__).stem + '.png')
+
+plt.savefig(
+    fname = path_figure,
+    format="png",
+    bbox_inches='tight',
+    transparent = False
+)
